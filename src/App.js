7b467afed15fb9102cdfc3c209dfdd52f14fc578
Marine Khloyan <mkhloyan@gmail.com>
2022-08-23 19:31:21 +0400

    refactor 2

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
-import React, {useEffect, useState} from "react"
-import {useDispatch, useSelector} from "react-redux";
+import React from "react"
 
-import {isSidebarMenuOpen, setUsers} from "./store/slices/globalSlice";
 import Layout from "./components/Layout/Layout";
-import Sidebar from "./components/SideBar/Sidebar";
 import AppRoutes from "./Routes";
 
-import hamburger from './assets/menu-hamburger.svg'
-import './styles/helpers.scss';
-import './styles/header.scss';
-
 function App() {
-    const [sidebarOpen, setSidebarOpen] = useState(false)
-    const dispatch = useDispatch();
-    const usersData = useSelector(state => state.global.usersData);
-
-    useEffect(()=> {
-        dispatch(setUsers(usersData));
-    },[dispatch, usersData])
-
-    const handleOpenMenu = ()=> {
-        setSidebarOpen(!sidebarOpen);
-        dispatch(isSidebarMenuOpen(!sidebarOpen))
-    }
 
     return (
         <Layout>
-            <header>
-                <img alt='Menu Icon' src={hamburger} onClick={handleOpenMenu} />
-            </header>
-
-            <div className='flex main'>
-                <Sidebar data={usersData}/>
-                <AppRoutes />
-            </div>
+            <AppRoutes />
         </Layout>
     );
 }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,38 @@
-import React  from "react"
+import React, {useEffect, useState} from "react"
+import {useDispatch, useSelector} from "react-redux";
+
+import {isSidebarMenuOpen, setUsers} from "../../store/slices/globalSlice";
+import hamburger from '../../assets/menu-hamburger.svg';
+import Sidebar from "../SideBar/Sidebar";
+import '../../styles/helpers.scss';
+import '../../styles/header.scss';
 
 function Layout({ children }) {
-    return <>{children}</>;
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+    const dispatch = useDispatch();
+    const usersData = useSelector(state => state.global.usersData);
+
+    useEffect(()=> {
+        dispatch(setUsers(usersData));
+    },[dispatch, usersData])
+
+    const handleOpenMenu = ()=> {
+        setSidebarOpen(!sidebarOpen);
+        dispatch(isSidebarMenuOpen(!sidebarOpen))
+    }
+
+    return (
+        <>
+            <header>
+                <img alt='Menu Icon' src={hamburger} onClick={handleOpenMenu} />
+            </header>
+
+            <div className='flex main'>
+                <Sidebar data={usersData}/>
+                {children}
+            </div>
+        </>
+    );
 }
 
 export default Layout;
